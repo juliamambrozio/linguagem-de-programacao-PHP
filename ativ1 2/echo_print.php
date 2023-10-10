32f0e3d4cd9b72e61244c0b4a700d5c7fb0ea31e
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2023-10-10 16:04:38 -0300

    echo e print

---
+++ ativ1 2/echo_print.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ECHO E PRINT</title>
+</head>
+<body>
+    <?php 
+    /* exibir */echo 'Comando echo<br/>'; 
+    echo '</br>';
+    /* função, podendo capturar um retorno*/print 'Comando print<hr/>';
+
+    /*  "echo print" retorna 1 quando deu certo e quando deu errado retorna 0 */
+    ?>
+</body>
+</html>
