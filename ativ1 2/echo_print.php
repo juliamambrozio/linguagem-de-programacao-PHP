148c7e9555799b66165a7df5d7fb6b55ededb8ea
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2023-10-11 18:26:21 -0300

    comentários

--- ativ1 2/echo_print.php
+++ ativ1 2/echo_print.php
@@ -6,12 +6,20 @@
     <title>ECHO E PRINT</title>
 </head>
 <body>
+    <!-- Comentários (é possível ler quando inspecionar)-->
     <?php 
     /* exibir */echo 'Comando echo<br/>'; 
     echo '</br>';
     /* função, podendo capturar um retorno*/print 'Comando print<hr/>';
 
     /*  "echo print" retorna 1 quando deu certo e quando deu errado retorna 0 */
+    
+    //Não é possível ler os comentários na hora de inspecionar
+    //Comentário de uma linha
+
+    # Comentário de uma linha
+
+
     ?>
 </body>
 </html>
