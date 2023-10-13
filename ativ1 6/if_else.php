79688b16f91c6f50ce871301e1659924a6aef748
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2023-10-13 10:06:46 -0300

    operadores de comparação

---
+++ ativ1 6/if_else.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>If e Else - Operadores Condicionais</title>
+    <?php 
+        //Comparação
+
+        /* == (iguais)
+        === (idênticos)
+        != (diferente)
+        <> (diferente)
+        !== (não idênticos)
+        < (menor)
+        > (maior)
+        <= (menor igual)
+        >= (maior igual)
+        */
+
+        if(2 == 2){
+            echo 'Verdadeiro';
+
+        } else{
+            echo 'Falso';
+        } //se tirar o else e a condição for falsa, o php vai desconsiderar o operador
+    ?>
+</head>
+<body>
+    
+</body>
+</html>
