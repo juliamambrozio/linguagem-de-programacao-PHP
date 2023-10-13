5ae2051abff3e87fcf15c2c92279376e82f03fc4
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2023-10-13 10:07:08 -0300

    if e else prática

---
+++ ativ1 8/if_else_praticando.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>If e Else - Praticando mais um pouco</title>
+    <?php
+
+    $usuario_possui_cartao_loja = true;
+    $valor_compra = 225;
+
+    $valor_frete = 50;
+    $recebeu_desconto_frete = false;
+
+    if($usuario_possui_cartao_loja == true && $valor_compra >= 100){
+        $valor_frete = 0;
+        $recebeu_desconto_frete = true;
+    }
+
+
+    ?>
+</head>
+<body>
+    <h1>Detalhes do pedido</h1>
+    <p>Possui cartão da loja?
+        <?php
+        if($usuario_possui_cartao_loja == true){
+            echo 'SIM';
+        } else{
+            echo 'NÃO';
+        }
+        ?></p>
+
+        <p>Valor da compra: <?= $valor_compra ?></p>
+
+        <p>Recebeu desconto no frete?
+        <?php
+        if($recebeu_desconto_frete == true){
+            echo 'SIM';
+        } else{
+            echo 'NÃO';
+        }
+        ?></p>
+
+        <p>Valor do frete: <?= $valor_frete ?></p>
+
+        <p>Valor total: <?= $valor_frete + $valor_compra?></p>
+</body>
+</html>
