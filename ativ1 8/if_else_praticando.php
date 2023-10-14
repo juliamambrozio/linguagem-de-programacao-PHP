a77fa76374f22878bc1156e2889e2cc8807d2daa
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2023-10-14 09:18:25 -0300

    condições encadeadas

--- ativ1 8/if_else_praticando.php
+++ ativ1 8/if_else_praticando.php
@@ -7,13 +7,20 @@
     <?php
 
     $usuario_possui_cartao_loja = true;
-    $valor_compra = 225;
+    $valor_compra = 1;
 
     $valor_frete = 50;
     $recebeu_desconto_frete = false;
 
-    if($usuario_possui_cartao_loja == true && $valor_compra >= 100){
+    if($usuario_possui_cartao_loja == true && $valor_compra >= 400){
         $valor_frete = 0;
+        $recebeu_desconto_frete = true;
+    } else if($usuario_possui_cartao_loja == true && $valor_compra >= 300){
+        $valor_frete = 10;
+        $recebeu_desconto_frete = true;
+
+    }else if($usuario_possui_cartao_loja == true && $valor_compra >= 100){
+        $valor_frete = 25;
         $recebeu_desconto_frete = true;
     }
 
