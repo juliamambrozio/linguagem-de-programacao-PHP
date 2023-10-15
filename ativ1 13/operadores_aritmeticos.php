59ea3d18cfb9ce87514e4c4cd8428eaa67eb8780
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2023-10-15 09:53:21 -0300

    operadores aritméticos

---
+++ ativ1 13/operadores_aritmeticos.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores Aritméticos</title>
+
+    <?php
+    $num1 = 16;
+    $num2 = 4;
+
+    echo "A soma entre $num1 e $num2 é ". ($num1 + $num2);
+
+    echo '<br/>';
+
+    echo "A subtração entre $num1 e $num2 é ". ($num1 - $num2);
+
+    echo '<br/>';
+
+    echo "A multiplicação entre $num1 e $num2 é ". ($num1 * $num2);
+
+    echo '<br/>';
+
+    echo "A divisão entre $num1 e $num2 é ". ($num1 / $num2);
+
+    echo '<br/>';
+
+    echo "O módulo entre $num1 e $num2 é ". ($num1 % $num2); //sobra da divisão
+    ?>
+</head>
+<body>
+    
+</body>
+</html>
