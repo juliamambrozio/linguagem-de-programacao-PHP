a378cf92e65ef88866788a36e216cd3f09d859e1
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2023-10-16 17:54:26 -0300

    operações aritméticas na atribuição de valores

---
+++ ativ1 14/operadores_atribuicao_aritmeticos.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operações Aritméticas na Atribuição de valores</title>
+    <?php
+
+    $x = 10;
+    $x = $x + 5;
+    $y = 8;
+    //OU
+   // $x += 5; //soma e atribui, dá para fazer isso com todos os  sinais
+
+    $x += $y;
+
+    echo $x;
+    ?>
+</head>
+<body>
+    
+</body>
+</html>
