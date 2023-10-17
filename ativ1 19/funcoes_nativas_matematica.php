049a6160c8859af6be5466ac776459b2918d47e3
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2023-10-17 16:52:43 -0300

    funções nativas matemática

---
+++ ativ1 19/funcoes_nativas_matematica.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funções nativas para tarefas matemáticas</title>
+    <?php
+
+        //$num = 7.8;
+        //echo ceil($num); //arredonda para cima (para um número inteiro)
+
+        //echo '<br />';
+
+        //echo floor($num); //arredonda para baixo (para um número inteiro)
+
+        //echo '<br />';
+
+        //echo round($num); //arredonda com base na fração, abaixo de 0.5 vai para baixo e acima de 0.5 vai para cima
+
+        //echo '<br />';
+
+        //echo rand(10, 20); //gerar valor aleatório, 0 até randmax
+        //echo '<br />';
+        //echo getrandmax(); //número máximo para o sistema operacional
+
+        //echo '<br />';
+
+        echo sqrt(9) //retorna a raiz quadrada do número
+    ?>
+</head>
+<body>
+    
+</body>
+</html>
