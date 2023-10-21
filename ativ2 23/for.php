c5867ffc51af0331cd86be4c3f407ad6ebe87c4b
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2023-10-21 10:47:08 -0300

    for

---
+++ ativ2 23/for.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>For</title>
+    <?php
+    /*
+    while(condicao){
+
+    }
+
+    do{
+
+    } while();
+    
+    */ 
+        for($x = 1; $x <= 10; $x++){ //podemos omitir a condição e fazer dentro do laço
+            //break
+            //continue
+
+            echo "$x </br>";
+
+        }
+
+    ?>
+</head>
+<body>
+    
+</body>
+</html>
